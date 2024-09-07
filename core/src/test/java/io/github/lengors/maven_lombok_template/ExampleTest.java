5c87940ef4cd1fee8d67da6bcfb688e5ebabb8c3
lengors <24527258+lengors@users.noreply.github.com>
2024-09-07 11:20:38 +0000

    💡 Remove unnecessary javadoc comments

--- core/src/test/java/io/github/lengors/maven_lombok_template/ExampleTest.java
+++ core/src/test/java/io/github/lengors/maven_lombok_template/ExampleTest.java
@@ -4,15 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.ReflectionUtils;
 
-/**
- * Example test suite for {@link Example}.
- *
- * @author lengors
- */
 class ExampleTest {
-  /**
-   * Example test for {@link Example#Example()}.
-   */
   @Test
   void shouldThrowUnsupportedOperationExceptionWhenInstantiateExample() {
     Assertions.assertThrows(UnsupportedOperationException.class, () -> {
@@ -20,17 +12,11 @@ class ExampleTest {
     });
   }
 
-  /**
-   * Example test for {@link Example#example()}.
-   */
   @Test
   void shouldGetHelloWorldFromExampleFunction() {
     Assertions.assertEquals("Hello, World!", Example.example());
   }
 
-  /**
-   * Example test for {@link Example#main(String[])}.
-   */
   @Test
   void shouldExecuteMainFunctionWithoutErrors() {
     Example.main(new String[] {});
